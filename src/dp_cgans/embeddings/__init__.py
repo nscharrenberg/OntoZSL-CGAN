367176e4db7ee57ca5a838ca5a10f616b76aeef3
Noah Scharrenberg <nscharrenberg@hotmail.com>
2023-05-03 19:39:59 +0200

    Create Ontology Embeddings

---
+++ src/dp_cgans/embeddings/__init__.py
@@ -0,0 +1,4 @@
+from dp_cgans.embeddings.utils import log
+from dp_cgans.embeddings.projections import create_projection, project, ProjectionType
+from dp_cgans.embeddings.random_walks import create_random_walker, walk, WalkerType
+from dp_cgans.embeddings.embed import embed, load_embedding
